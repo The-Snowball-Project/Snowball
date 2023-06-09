eae8461ba2dc79141037b7f785e6eca72b08947b
alturios <paul.kosmala@gmail.com>
2023-06-08 17:52:57 -0700

    clean and ready for pr

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 import * as express from 'express';
-import {Request,Response} from "express";
-const app = express()
-
+const app = express();
 const port:string = process.env.PORT || "3000";
-
-app.get('/', (req:Request, res: Response) => {
-  res.send('')
-})
-
+app.use(express.static(__dirname + '/public'));
 app.listen(port, () => {
   console.log(`testing listening on port ${port}`)
 })
