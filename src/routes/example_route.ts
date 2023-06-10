8ccfe704c69a4a38c98eb7706eb4b2dc67600b12
alturios <paul.kosmala@gmail.com>
2023-06-10 13:42:12 -0700

    working test

--- src/routes/example_route.ts
+++ src/routes/example_route.ts
@@ -1,6 +1,26 @@
-const route = ['/','GET',(req:Request,res:any)=>{
-    console.log("hi!")
-    res.send("hello there");
+import { response } from 'express';
+import pool from '../dbconfig/dbconfig';
+const route = ['/','GET',  (req:Request,res:any)=>{
+    try {
+        const sql = "SELECT * FROM test";
+        const params = null
+        pool
+            .connect()
+            .then((db:any)=>db
+                .query(sql,params,(error:Error,data:any)=>{
+                    if (error) throw error
+                    res.status(200)
+                        .send(data.rows);
+                    db.release();
+                    res.end();
+
+            }));
+    } catch (error) {
+        res.status(400)
+            .send(error);
+        res.end();
+
+    }
 }];
 
 export default route;
