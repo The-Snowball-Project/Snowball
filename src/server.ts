1217270f4c348aefb84ebe79edb933fcda762331
altruios <49083558+altruios@users.noreply.github.com>
2023-06-10 19:21:30 -0700

    Update src/server.ts
    Co-authored-by: JM <70044486+jm-alan@users.noreply.github.com>

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-export default (function server(){
-    const express = require('express')
-    const app = express()
-    const path = require("path")
-    app.use(express.static(path.join(__dirname,"public")))
-    app.use(express.json())
-    app.use(express.urlencoded({ extended: false }))
-    return app;
-})()
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+export default app;
