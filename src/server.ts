94f3c128f053582458e783c1771a8166bbda5133
alturios <paul.kosmala@gmail.com>
2023-06-12 12:11:45 -0700

    helmet and cors added (un-configured)

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import path from 'path';
-
+import cors from 'cors';
+import helmet from "helmet";
 const app = express();
 
+app.use(cors());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
