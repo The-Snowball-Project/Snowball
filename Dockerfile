7af6cc8f007ae2f41f44777a35b371ecb1b226dc
Brody <reed.thorngag@gmail.com>
2023-06-18 21:29:22 +1200

    prisma working along with POC user add and fetch all users

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,7 @@ WORKDIR /app
 
 COPY . .
 
-RUN npm i
-
-ENV DB_URL postgresql://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_PATH}
-
-RUN npx prisma generate
+#RUN npm i
 
 FROM base as production
 
--- src/schemaTest.ts
+++ src/schemaTest.ts
@@ -1,8 +1,22 @@
+import { PrismaClient } from '@prisma/client'
+import logger from './util/logger'
+import { json } from 'express';
 
-function test() {
+const prisma = new PrismaClient()
 
+
+async function test() {
+    logger.info("Adding new test user with name: 'test'");
+    await prisma.user.create({
+        data: {
+            Name: "test",
+            Bio: ""
+        }
+    })
+    const data = await prisma.user.findMany();
+    logger.info("all users in users table:\n\tUser.Name: "+data.map((user) => user.Name).join('\n\tUser.Name: '));
 }
-
+ 
 export default test;
 
 
